239f4b65901a4142fcb1f5b27f19f85d8697e578
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-26 23:46:02 -0700

    refactor(decimals): change to a standar FromStr instead custom parse method in impl block
    Is not only more idiomatic, also allow to use it direclty with str::parse

--- src/decimal.rs
+++ src/decimal.rs
@@ -31,52 +31,57 @@ impl std::fmt::Display for Decimal<'_> {
     }
 }
 
-impl<'a> Decimal<'a> {
-    pub fn str_to_decimal(string: &'a str) -> Self {
+impl<'a> std::str::FromStr for Decimal<'a> {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut pre = String::new();
         let mut post = Vec::<u8>::new();
-
+        
         enum State {
             Pre,
             Post
         }
-
+        
         let mut state = State::Pre;
-
-        for ch in string.chars() {
+        
+        for ch in s.chars() {
             if !ch.is_ascii_digit() && ch != '.' {
                 panic!("{}", format!("'{ch}' is not valid digit"))
             }
-
+        
             match state {
                 State::Pre => {
                     if ch == '.' {
                         state = State::Post;
-
+        
                         continue;
                     }
-
+        
                     pre.push(ch);
                 }
                 State::Post => {
                     if ch == '.' {
                         panic!("insert more than one '.'")
                     }
-
+        
                     post.push(ch.to_string().parse().unwrap());
                 }
             }
         }
-
-        Self { 
+        
+        Ok(Self { 
             pre: pre.parse().unwrap(),
             post: Box::leak(post.into_iter().rev().collect::<Vec<u8>>().into_boxed_slice())
-         }
+        })
     }
 }
 
+// impl<'a> Decimal<'a> {}
+
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
     use super::*;
 
     #[test]
@@ -84,11 +89,11 @@ mod tests {
         let cases = [
             ("1.45", "1.45", Decimal { pre: 1, post: &[5, 4] }),
             ("1.", "1.0", Decimal { pre: 1, post: &[] }),
-            ("1", "1.0", Decimal { pre: 1, post: &[] })
+            ("1", "1.0", Decimal { pre: 1, post: &[]  })
         ];
 
         for (input_num, expected_str, expected_decimal) in cases {
-            let decimal = Decimal::str_to_decimal(input_num);
+            let decimal  = Decimal::from_str(input_num).unwrap();
 
             assert_eq!(decimal, expected_decimal);
             assert_eq!(format!("{decimal}"), expected_str);
