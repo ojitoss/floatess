3aff706800d924c140a2edd30b6d57aac56c10c0
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-28 11:10:20 -0700

    feat(storage): support to basic u8 wihtout slice

--- src/digit_storage.rs
+++ src/digit_storage.rs
@@ -16,3 +16,21 @@ impl DigitStorage<u8> for [u8] {
         }
     }
 }
+
+impl DigitStorage<u8> for u8 {
+    fn len_digits(&self) -> u32 {
+        if *self == 0 { return 0 };
+
+        self.ilog10() + 1
+    }
+
+    fn get_digit(&self, index: usize) -> Option<u8> {
+        let len = self.len_digits();
+
+        if index > len as usize { None? }
+
+        let n = 10u8.pow(len - 1 - index as u32);
+
+        Some((*self / n) % 10)
+    }
+}
