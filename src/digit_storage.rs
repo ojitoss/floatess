e940f079491c8ede71343740d8f715cbe536d70f
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-28 11:18:09 -0700

    feat(storage): change firm of trait to return always a usize
    This because in ops modules was not need more transformations

--- src/digit_storage.rs
+++ src/digit_storage.rs
@@ -1,36 +1,36 @@
-pub trait DigitStorage<T> {
-    fn len_digits(&self) -> u32;
+pub trait DigitStorage {
+    fn len_digits(&self) -> usize;
 
-    fn get_digit(&self, index: usize) -> Option<T>;
+    fn get_digit(&self, index: usize) -> Option<usize>;
 }
 
-impl DigitStorage<u8> for [u8] {
-    fn len_digits(&self) -> u32 {
-        self.len() as u32
+impl DigitStorage for [u8] {
+    fn len_digits(&self) -> usize {
+        self.len() as usize
     }
 
-    fn get_digit(&self, index: usize) -> Option<u8> {
+    fn get_digit(&self, index: usize) -> Option<usize> {
         match self.get(index) {
-            Some(n) => Some(*n),
+            Some(n) => Some(*n as usize),
             None => None
         }
     }
 }
 
-impl DigitStorage<u8> for u8 {
-    fn len_digits(&self) -> u32 {
+impl DigitStorage for u8 {
+    fn len_digits(&self) -> usize {
         if *self == 0 { return 0 };
 
-        self.ilog10() + 1
+        self.ilog10() as usize + 1
     }
 
-    fn get_digit(&self, index: usize) -> Option<u8> {
+    fn get_digit(&self, index: usize) -> Option<usize> {
         let len = self.len_digits();
 
-        if index > len as usize { None? }
+        if index > len { None? }
 
-        let n = 10u8.pow(len - 1 - index as u32);
+        let n = 10u8.pow((len - 1 - index) as u32);
 
-        Some((*self / n) % 10)
+        Some(((*self / n) % 10) as usize)
     }
 }
