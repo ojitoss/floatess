c832c32c2d654274e561359b06909a8eea6fd210
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-27 23:33:14 -0700

    perf(decimal/ops/add): remove a reverse iter method of result of calc

--- src/decimal/ops/add.rs
+++ src/decimal/ops/add.rs
@@ -7,7 +7,7 @@ impl<'a> Add for Decimal<'a> {
     fn add(self, rhs: Self) -> Self::Output {
         let mut add_one_next = false;
         let max_len = usize::max(self.post.len(), rhs.post.len());
-        let mut res = Vec::<u8>::with_capacity(max_len);
+        let mut res = vec![0; max_len];
 
         for i in (0..max_len).rev() {
             let left = self.post.get(i);
@@ -16,7 +16,7 @@ impl<'a> Add for Decimal<'a> {
             if left.is_none() || right.is_none() {
                 let value = if right.is_none() { *left.unwrap() } else { *right.unwrap() };
 
-                res.push(value);
+                res[i] = value;
 
                 continue;
             }
@@ -26,16 +26,16 @@ impl<'a> Add for Decimal<'a> {
 
             let sum = left + right + add_one_next as u8;
             let low_ten = sum < 10;
-            let to_push = sum - (10 * (!low_ten as u8));
+            let value = sum - (10 * (!low_ten as u8));
 
-            res.push(to_push);
+            res[i] = value;
 
             add_one_next = !low_ten;
         }
 
         Self { 
             pre: self.pre + rhs.pre + add_one_next as u32,
-            post: Box::leak(res.into_iter().rev().collect::<Vec<u8>>().into_boxed_slice())
+            post: Box::leak(res.into_boxed_slice())
          }
     }
 }
