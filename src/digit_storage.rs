5966835ab472698853f234d211d0bd984c8cc1fb
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-29 23:15:17 -0700

    feat(storage): add support for all slices

--- src/digit_storage.rs
+++ src/digit_storage.rs
@@ -3,30 +3,43 @@ pub trait DigitStorage {
 
     fn get_digit(&self, index: usize) -> Option<usize>;
 
-    fn from_slice<'a>(slice: &'a [u8]) -> Self;
+    fn from_slice(slice: &[u8]) -> Self;
 }
 
-impl DigitStorage for &[u8] {
-    fn len_digits(&self) -> usize {
-        self.len()
-    }
-
-    fn get_digit(&self, index: usize) -> Option<usize> {
-        match self.get(index) {
-            Some(n) => Some(*n as usize),
-            None => None
-        }
-    }
-
-    fn from_slice(slice: &[u8]) -> Self {
-        let slice  = Vec::from(slice);
-
-        Box::leak(slice.into_boxed_slice())
-    }
+macro_rules! impl_ints_slices {
+    ( $( $type:ty );* $(;)? ) => {
+        $(
+            impl DigitStorage for &[$type] {
+                fn len_digits(&self) -> usize {
+                    self.len()
+                }
+            
+                fn get_digit(&self, index: usize) -> Option<usize> {
+                    match self.get(index) {
+                        Some(n) => Some(*n as usize),
+                        None => None
+                    }
+                }
+            
+                fn from_slice(slice: &[u8]) -> Self {
+                    let slice = Vec::from(slice).into_iter()
+                        .map(| x | x as $type)
+                        .collect::<Vec<$type>>();
+            
+                    Box::leak(slice.into_boxed_slice())
+                }
+            }
+        )*
+    };
 }
+
+impl_ints_slices!(
+    u8; u16; u32; u64; u128; usize;
+    i8; i16; i32; i64; i128; isize
+);
 
 macro_rules! impl_ints {
-    ($( $type:ty );* $(;)? ) => {
+    ( $( $type:ty );* $(;)? ) => {
         $(
             impl DigitStorage for $type {
                 fn len_digits(&self) -> usize {
