34d9f36888e6f4a99fe39cb4018a182b01973427
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-31 14:53:06 -0700

    refactor(core/storage): remove signed ints from the implementation
    The DigitStorage trait is based on a digit stream 0..9, so signed no had sense bcs a digit themselves can't be negative.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -72,10 +72,7 @@ macro_rules! impl_ints {
     };
 }
 
-impl_ints!(
-    u8; u16; u32; u64; u128; usize;
-    i8; i16; i32; i64; i128; isize
-);
+impl_ints!(u8; u16; u32; u64; u128; usize);
 
 impl DigitStorage for () {
     fn len_digits(&self) -> usize { 0 }
