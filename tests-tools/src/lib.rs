d929a51a91d77db98d762ebac4523eafbadd496e
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-09-01 21:52:31 -0700

    refactor(test-tools): change generic ident to impl block to be more explicit

--- tests-tools/src/lib.rs
+++ tests-tools/src/lib.rs
@@ -12,13 +12,13 @@ pub struct CaseOp<'a, Lhs, Rhs, Exp> {
     pub desc: &'a str
 }
 
-impl<'a, L, R, E> CaseOp<'a, L, R, E>
+impl<'a, Lhs, Rhs, Exp> CaseOp<'a, Lhs, Rhs, Exp>
 where
-    L: Add<R, Output = E> + Debug,
-    R: Debug,
-    E: PartialEq + Debug
+    Lhs: Add<Rhs, Output = Exp> + Debug,
+    Rhs: Debug,
+    Exp: PartialEq + Debug
 {
-    pub fn add<F>(self, callback: F) where F: Fn(&L, &R, &E, &str) -> String {
+    pub fn add<F>(self, callback: F) where F: Fn(&Lhs, &Rhs, &Exp, &str) -> String {
         let desc = callback(&self.lhs, &self.rhs, &self.expected, self.desc);
 
         assert_eq!(self.lhs + self.rhs, self.expected, "{}", failed_template(&desc));
