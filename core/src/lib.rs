d7b3e30992f417635c2242008caa4b35396ec36a
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-09-01 21:40:44 -0700

    feat(core/stream): change unit impl for a custom ZST newtype

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,14 +4,12 @@ pub trait DigitsStream {
     fn len_digits(&self) -> usize;
 
     fn get_digit(&self, index: usize) -> Option<usize>;
-
-    fn from_slice(slice: &[u8]) -> Self;
 }
 
-impl DigitsStream for () {
+struct WithoutDigitsStream;
+
+impl DigitsStream for WithoutDigitsStream {
     fn len_digits(&self) -> usize { 0 }
 
     fn get_digit(&self, _index: usize) -> Option<usize> { None }
-
-    fn from_slice(_slice: &[u8]) -> Self {}
 }
