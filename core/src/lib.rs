d54e7674d3b3db9374f879f7709df1c97f01f4da
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-09-01 21:59:02 -0700

    refactor(core/stream): move DigitsStream struct to stream module file manager insetad on root of lib.rs file
    The main reason of this was than the void struct of stream, can be exported for stream submodule instead of root.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,23 +1,3 @@
 pub mod stream;
 
-pub trait DigitsStream {
-    fn len_digits(&self) -> usize;
-
-    fn get_digit(&self, index: usize) -> Option<usize>;
-}
-
-struct WithoutDigitsStream;
-
-impl DigitsStream for WithoutDigitsStream {
-    fn len_digits(&self) -> usize { 0 }
-
-    fn get_digit(&self, _index: usize) -> Option<usize> { None }
-}
-
-impl TryFrom<&[u8]> for WithoutDigitsStream {
-    type Error = ();
-
-    fn try_from(_value: &[u8]) -> Result<Self, Self::Error> {
-        Ok(WithoutDigitsStream)
-    }
-}
+pub use stream::DigitsStream;
--- core/src/stream.rs
+++ core/src/stream.rs
@@ -3,3 +3,25 @@ mod basic;
 
 pub use small::SmallDigitsStream;
 pub use basic::BasicDigitsStream;
+
+pub trait DigitsStream {
+    fn len_digits(&self) -> usize;
+
+    fn get_digit(&self, index: usize) -> Option<usize>;
+}
+
+struct WithoutDigitsStream;
+
+impl DigitsStream for WithoutDigitsStream {
+    fn len_digits(&self) -> usize { 0 }
+
+    fn get_digit(&self, _index: usize) -> Option<usize> { None }
+}
+
+impl TryFrom<&[u8]> for WithoutDigitsStream {
+    type Error = ();
+
+    fn try_from(_value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(WithoutDigitsStream)
+    }
+}
