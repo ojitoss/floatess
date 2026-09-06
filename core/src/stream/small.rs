db5973fc40821ce19c70dd5147618cd8560399d1
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-09-06 10:23:42 -0700

    test(core/stream/small): add case in from slice unit about so high amount of digits for this unsigned int

--- core/src/stream/small.rs
+++ core/src/stream/small.rs
@@ -111,5 +111,6 @@ mod tests {
         assert_eq!(SmallDigitsStream::<u32>::try_from(*&[1, 0, 0, 0].as_slice()), Ok(SmallDigitsStream(1000)));
         assert_eq!(SmallDigitsStream::<u32>::try_from(*&[1, 2, 3].as_slice()), Ok(SmallDigitsStream(123)));
         assert_eq!(SmallDigitsStream::<u8>::try_from(*&[1, 2, 10].as_slice()), Err(SmallDigitsStreamError::HighThanNine { index: 2 }));
+        assert_eq!(SmallDigitsStream::<u8>::try_from(*&[1, 2, 3, 4].as_slice()), Err(SmallDigitsStreamError::OverflowDigitsAmount))
     }
 }
